699a3f01988607c5ab211ca50c33c83dee0d6100
Zain Abbas <45133494+zain-astrophysics@users.noreply.github.com>
2025-02-26 15:04:46 -0500

    Update Apache_mapper.py

--- Apache_mapper_reducer/Apache_mapper.py
+++ Apache_mapper_reducer/Apache_mapper.py
@@ -8,11 +8,7 @@ from apachelogs import LogParser
 url = "https://raw.githubusercontent.com/singhj/big-data-repo/refs/heads/main/datasets/access.log"
 response = requests.get(url)
 
-# Initialize counters
-GET_count = 0
-PUT_count = 0
-POST_count = 0
-total_requests = 0
+
 
 # Define the log format (example for the common log format)
 parser = LogParser('%h %l %u %t "%r" %>s %b "%{Referer}i" "%{User-Agent}i" "%{Referer}i"')
