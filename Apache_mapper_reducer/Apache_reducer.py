795cc7d0c3b318e91e12a84b0d24229eafd9538b
Zain Abbas <45133494+zain-astrophysics@users.noreply.github.com>
2025-02-26 14:28:56 -0500

    Update Apache_reducer.py

--- Apache_mapper_reducer/Apache_reducer.py
+++ Apache_mapper_reducer/Apache_reducer.py
@@ -1,6 +1,22 @@
 #!/usr/bin/env python
 
 import sys
-current_president = None
-total_valence = 0
-word_count = 0
+
+
+for line in sys.stdin:
+  if line.strip():  # Skip empty lines
+        total_requests += 1
+        GET_count += line.count('GET')
+        PUT_count += line.count('PUT')
+        POST_count += line.count('POST')
+
+# Calculate the percentage of GET requests
+percentage_get_requests = (GET_count / total_requests) * 100
+percentage_put_requests = (PUT_count / total_requests) * 100
+percentage_post_requests = (POST_count / total_requests) * 100
+
+# Display the result
+print(f"Percentage of GET requests: {percentage_get_requests:.2f}%")
+print(f"Percentage of PUT requests: {percentage_put_requests:.2f}%")
+print(f"Percentage of POST requests: {percentage_post_requests:.2f}%")
+
